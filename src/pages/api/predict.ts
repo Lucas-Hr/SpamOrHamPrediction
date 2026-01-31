fcdf9d03e7a20f2853839c3315b80eab4ccfc2ae
Mirantsoa <mirantsoarabeantoandro@gmail.com>
2026-01-31 20:21:03 +0300

    Update backend URL

--- src/pages/api/predict.ts
+++ src/pages/api/predict.ts
@@ -1,6 +1,6 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 
-const BACKEND_URL = process.env.BACKEND_URL || "https://votre-backend.up.railway.app";
+const BACKEND_URL = process.env.BACKEND_URL || "https://backendspamorham.up.railway.app";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method !== 'POST') {
