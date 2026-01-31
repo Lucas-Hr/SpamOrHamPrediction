8a9c06e41e65add14fa0bf6ef898a7f555843699
Lucas Henintsoa <126322847+Lucas-Hr@users.noreply.github.com>
2026-01-31 22:25:36 +0300

    fixed flex

--- src/components/result.tsx
+++ src/components/result.tsx
@@ -14,24 +14,35 @@ export default function Result({prediction, precisionHam, precisionSpam,confiden
       onClick={() => setIsVisible(false)}
       >&times;</span>
       <h2 className="text-2xl font-semibold text-center">{(precisionSpam ?? 0) > (precisionHam ?? 0) ? "Spam" : "Ham"}</h2>
-      <p className="text-center text-gray-500 text-extralight mt-4">Probabilité Ham</p>
-      <AnimatedCircularProgressBar
-        value={(precisionHam ?? 0) * 100 || 20}
-        gaugePrimaryColor="rgba(62, 178, 255, 1)"
-        gaugeSecondaryColor="rgba(0, 0, 0, 0.1)"
-      />
-      <p className="text-center text-gray-500 text-extralight mt-4">Probabilité Spam</p>
+      <div className="flex justify-between items-center mt-4">
+        <div>
+              <p className="text-center text-gray-500 text-extralight">Probabilité Ham</p>
+            <AnimatedCircularProgressBar
+              value={(precisionHam ?? 0) * 100 || 20}
+              gaugePrimaryColor="rgba(62, 178, 255, 1)"
+              gaugeSecondaryColor="rgba(0, 0, 0, 0.1)"
+            />
+        </div>
+        <div>
+        
+      <p className="text-center text-gray-500 text-extralight">Probabilité Spam</p>
       <AnimatedCircularProgressBar
         value={(precisionSpam ?? 0) * 100 || 20}
         gaugePrimaryColor="rgba(255, 107, 107, 1)"
         gaugeSecondaryColor="rgba(0, 0, 0, 0.1)"
       />
-      <p className="text-center text-gray-500 text-extralight mt-4">Pourcentage de confiance</p>
+        </div>
+        <div>
+        
+      <p className="text-center text-gray-500 text-extralight">Pourcentage de confiance</p>
       <AnimatedCircularProgressBar
         value={(confidence ?? 0) || 20}
         gaugePrimaryColor="rgba(255, 249, 62, 1)"
         gaugeSecondaryColor="rgba(0, 0, 0, 0.1)"
       />
+        </div>
+      </div>
+
     </div>
   );
 }
